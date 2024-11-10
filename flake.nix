4b046e80a1b3942b9d2774db83db839b352baaf2
Antonio Teixeira <antonio.teixeira@gamancorp.com>
2024-11-10 14:55:11 +0000

    rewrite flake based on template

--- flake.nix
+++ flake.nix
@@ -1,20 +1,21 @@
 {
-
-  # Packages Sources
-  inputs = {
-
-    # Base URL
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
-
+  description = "Flake For Docs";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+  inputs.systems.url = "github:nix-systems/default";
+  inputs.flake-utils = {
+    url = "github:numtide/flake-utils";
+    inputs.systems.follows = "systems";
   };
 
-  # Deployed Packages
-  outputs = { ... }: {
-
-    # Development Env
-    devShells.default = pkgs.mkShell {
-      packages = with pkgs; [ nodejs yarn];
-    };
-
-  };
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell { packages = [ pkgs.nodejs_22 ]; };
+      }
+    );
 }
